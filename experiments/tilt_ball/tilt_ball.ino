963d94e1a1d80308163a85667fb98be81af5dfee
Tohma-desu <tomatonohayashi@gmail.com>
2026-06-10 23:10:00 -0700

    Add tilt ball switch experiment

---
+++ experiments/tilt_ball/tilt_ball.ino
@@ -0,0 +1,16 @@
+const int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(2, INPUT);
+  digitalWrite(2, HIGH);
+}
+
+void loop() {
+  int digitalVal = digitalRead(2);
+  if (digitalVal == HIGH) {
+    digitalWrite(ledPin, LOW);
+  } else {
+    digitalWrite(ledPin, HIGH);
+  }
+}
