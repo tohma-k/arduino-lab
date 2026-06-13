fb2ba0ede06818c7a2d82107fe6a7087d235917f
Tohma-desu <tomatonohayashi@gmail.com>
2026-06-13 09:56:09 -0700

    Add passive buzzer experiment

---
+++ experiments/passive_buzzer/passive_buzzer.ino
@@ -0,0 +1,20 @@
+#include "pitches.h"
+
+void setup() {
+}
+
+void loop() {
+  tone(8, NOTE_G5, 250);
+  delay(250);
+  tone(8, NOTE_A5, 250);
+  delay(250);
+  tone(8, NOTE_G5, 250);
+  delay(250);
+  tone(8, NOTE_F5, 250);
+  delay(250);
+  tone(8, NOTE_D5, 250);
+  delay(250);
+  tone(8, NOTE_F5, 250);
+
+  delay(2000);
+}
