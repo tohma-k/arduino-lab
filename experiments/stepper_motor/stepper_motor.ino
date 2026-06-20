960dde6743b5f038e8add7d3ecb44210c80c8ee2
Tohma-desu <tomatonohayashi@gmail.com>
2026-06-19 23:05:58 -0700

    Add stepper motor experiment

---
+++ experiments/stepper_motor/stepper_motor.ino
@@ -0,0 +1,21 @@
+#include <Stepper.h>
+
+const int stepsPerRevolution = 2048;
+const int rolePerMinute = 10;
+
+Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
+
+void setup() {
+  myStepper.setSpeed(rolePerMinute);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("clockwise");
+  myStepper.step(stepsPerRevolution);
+  delay(500);
+
+  Serial.println("counterclockwise");
+  myStepper.step(-stepsPerRevolution);
+  delay(500);
+}
